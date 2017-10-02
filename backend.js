74a1f64e12acaff23a728ed6fde8ef22a462aa40
Neil Manvar <ndmanvar@yahoo.com>
2017-10-01 22:49:24 -0700

    cleaning up

--- backend.js
+++ backend.js
@@ -40,9 +40,6 @@ app.get('/getevents/:session', function (req, res) {
 
   connection.query('SELECT * FROM events WHERE event_uid = ?', req.params.session, function (error, results, fields) {
     if (error) throw error; // Need to implement proper error handler, otherwise request will show as pending
-
-      console.log(results);
-
       res.writeHead(200, { 'Content-Type': 'application/json' });
       res.end(JSON.stringify(results));
   });
@@ -53,7 +50,6 @@ app.get('/getsafe/:session', function (req, res) {
 
   connection.query('SELECT * FROM events WHERE event_uid = ? and event_safe = 0', req.params.session, function (error, results, fields) {
     if (error) throw error; // Need to implement proper error handler, otherwise request will show as pending
-
       res.writeHead(200, { 'Content-Type': 'application/json' });
       res.end(JSON.stringify({
         safe: !results.length
