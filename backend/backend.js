d678e11cb4943353ce9fcd04240d641904361bba
Sasha Sharma <ndmanvar@Neils-iMac.local>
2017-10-08 22:34:45 -0700

    reset password

--- backend/backend.js
+++ backend/backend.js
@@ -5,7 +5,7 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'paassword',
+  password: 'password',
   database: 'no-more-qa'
 });
 
