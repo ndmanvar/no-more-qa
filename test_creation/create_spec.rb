ca3ae8f84f83a8df9cf5fe3ae4be8514a01f5bd2
Neil Manvar <ndmanvar@yahoo.com>
2017-12-17 00:34:17 -0800

    finally flattened and working

--- test_creation/create_spec.rb
+++ test_creation/create_spec.rb
@@ -9,7 +9,7 @@ require 'json'
 require 'uri'
 require 'net/http'
 
-url = URI("https://api.sessionstack.com/v1/websites/2319/sessions/5a12543e7a7c26a22ac0747b/export")
+url = URI("https://api.sessionstack.com/v1/websites/2319/sessions/5a3619c27d41c305749a0fac/export") # TO DO: replace with correct session
 
 http = Net::HTTP.new(url.host, url.port)
 http.use_ssl = true
@@ -24,17 +24,37 @@ response = http.request(request)
 response = JSON.parse(response.read_body)
 events = response['events']
 
-initialState_nodes = response['initialState']['snapshot']['childNodes']
+$initialState_nodes = response['initialState']['snapshot']['childNodes'] # TODO: childNodes might not always exist like this
+
+def flatten(nodes)
+    counter = 0
+    
+    while counter < nodes.size do
+        current_node = nodes[counter]
+        if current_node['childNodes'] != nil
+            for node in current_node['childNodes']
+                nodes.push(node)
+            end
+            current_node.delete('childNodes')
+        end
+        counter += 1
+    end
+    return nodes
+end
+
+$initialState_nodes = flatten($initialState_nodes) # TODO: use !
 
 def get_sessionstack_node(sessionstack_id)
         
     # USE RECURISION!!! NEIL IS HERE
     # OR maybe not use recursion
     # use sessionstack_id as HASH function. if sessionstack_id < or == or > curr_id. i.e. be smart about it
-    counter = 0
-    nodes = initialState_nodes
-    while (nodes[counter]['id'] != )
+
+    # need to get smarter about searching
+    for node in $initialState_nodes
+        return node if node["id"] == sessionstack_id
     end
+    return nil
 end
 
 test_location = "generated_tests/freshly_created_test.rb"
@@ -47,24 +67,29 @@ File.open(test_location, "w") do | file |
         spec_file.write "launch_browser #{url}\n"
 
         last_key_testId = nil
+
+        puts "events is #{events}"
         for event in events
             case event['type']
             when 'mouse_click'
-                id = event['data']['selector']['id'] if event['data']['selector'].nil?
+                # require 'pry'; binding.pry
+                id = event['data']['selector'][0]['id']
                 spec_file.write "click #{id}\n" unless id.nil?
             when 'dom_element_value_change'
-                # id = event['data']['selector']['id'] if event['data']['selector'].nil?
-                sessionstack_id = event['data']['id']
+                id = event['data']['id']
+
+                node = get_sessionstack_node(id)
+                id_attr = node['attributes'].find { | elem | elem['name'] == 'id' }
                 
                 if id.nil?
-                    puts "No idea detected for #{event['data']}" # TODO: figure out what to do for not "identified" elements
+                    puts "No id detected for #{event['data']}" # TODO: figure out what to do for not "identified" elements
                 else
                     if last_key_testId == id
                         spec_file.truncate(spec_file.size - 1) # remove last character (newline)
                         spec_file.pos -= 1
                         spec_file.write("#{event['data']['value']}\n")
                     else
-                        spec_file.write "key #{id} #{event['data']['value']}\n"
+                        spec_file.write "key #{id_attr['value']} #{event['data']['value']}\n"
                     end
                     last_key_testId = id
                 end
